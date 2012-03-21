9fb7265173055226a77e7ccf51a7168014a8a578
Dwayne Bailey <dwayne@translate.org.za>
2012-03-21 12:58:23 +0000

    Recover a language where we only have Mozilla files.

---
+++ recover-lang.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+langs=$*
+
+export PYTHONPATH=../tools/translate:$PYTHONPATH
+export PATH=../tools/translate/translate/convert:$PATH
+
+for lang in $langs
+do
+	moz2po -t ../l10n/en-US ../l10n/$lang $lang
+done
+
+./cleanup-msgcat.sh $langs
