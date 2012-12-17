e027d7dc2da0f57e69ffeb3f45cfb2c83b13faed
Dwayne Bailey <dwayne@translate.org.za>
2012-12-17 05:26:18 -0800

    Add doc comments

--- recover-lang-from-narro-zip.sh
+++ recover-lang-from-narro-zip.sh
@@ -3,13 +3,19 @@
 zip=$*
 lang=$(echo $zip | sed "s/Firefox Aurora-\(.*\)\.zip/\1/")
 
+# Make sure we have the Mercurial repo updated
 (cd ../l10n/; ./get-and-reset-lang.sh $lang)
 
+# Prepare the unzipped Narro files
 mkdir -p ../l10n/$lang
 unzip -u -d ../l10n/$lang "$zip"
 
+# Do a normal recovery
 ./recover-lang.sh $lang
 
+# Remove any non-PO files
 rm $(find $lang -type f | egrep -v "\.po$")
+
+# TODO update to latest templates
 
 pocount $lang | tail
