75cb54b7a9289af05130f069686627d5dfc45dde
Dwayne Bailey <dwayne@translate.org.za>
2012-12-20 04:16:48 -0800

    These reverts should happen for each language

--- remove-unchanged.sh
+++ remove-unchanged.sh
@@ -9,11 +9,13 @@ do
 	pofilter $progress -t unchanged $lang $lang-unchanged
 	podebug $progress --rewrite=blank $lang-unchanged $lang-unchanged-blank
 	pomerge $progress --mergecomments=no -t $lang $lang-unchanged-blank $lang
+
+	(cd $lang
+	git checkout \
+	browser/branding/official/brand.dtd.po \
+	browser/branding/official/brand.properties.po \
+	browser/chrome/browser-region/region.properties.po)
+
 	rm -rf $lang-unchanged*
 done
 
-cd $lang
-git checkout \
-browser/branding/official/brand.dtd.po \
-browser/branding/official/brand.properties.po \
-browser/chrome/browser-region/region.properties.po
