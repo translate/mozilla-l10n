3f2c4ba5a12f7e25641b1502766f094887f61473
Dwayne Bailey <dwayne@translate.org.za>
2012-12-20 05:18:02 -0800

    Use --mozilla to try catch more unchanged entries

--- remove-unchanged.sh
+++ remove-unchanged.sh
@@ -6,7 +6,7 @@ langs=$*
 
 for lang in $langs
 do
-	pofilter $progress -t unchanged $lang $lang-unchanged
+	pofilter --mozilla $progress -t unchanged $lang $lang-unchanged
 	podebug $progress --rewrite=blank $lang-unchanged $lang-unchanged-blank
 	pomerge $progress --mergecomments=no -t $lang $lang-unchanged-blank $lang
 
