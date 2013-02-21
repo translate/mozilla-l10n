448ebd03dc3645708f190dda8e7ff2e1ea319225
Dwayne Bailey <dwayne@translate.org.za>
2013-02-21 07:42:27 +0000

    Exclude some Mercurial noise

--- recover-lang.sh
+++ recover-lang.sh
@@ -7,7 +7,7 @@ export PATH=../tools/translate/translate/convert:$PATH
 
 for lang in $langs
 do
-	moz2po --progress=none -t ../l10n/en-US ../l10n/$lang $lang
+	moz2po --progress=none --exclude=".hgtags" -t ../l10n/en-US ../l10n/$lang $lang
 done
 
 ./cleanup-msgcat.sh $langs
