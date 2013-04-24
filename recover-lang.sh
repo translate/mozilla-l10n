c267f3540e66beb927d936546f0529ac66899296
Khaled Hosny <khaledhosny@eglug.org>
2013-04-24 10:25:40 +0200

    Make recover-lang.sh work again

--- recover-lang.sh
+++ recover-lang.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-source build_common.inc.sh
+source ttk.inc.sh
 
 langs=$*
 
-export PYTHONPATH=../tools/translate:$PYTHONPATH
-export PATH=../tools/translate/translate/convert:$PATH
+export PYTHONPATH=../translate:$PYTHONPATH
+export PATH=../translate/translate/convert:$PATH
 
 for lang in $langs
 do
-	moz2po --progress=none --exclude=".hgtags" -t templates-en-US ../l10n/$lang $lang
+	hg_lang=$(echo $lang | tr "_" "-")
+	moz2po --progress=none --exclude=".hgtags" -t templates-en-US build/l10n/$hg_lang $lang
 done
 rm $(find $lang -type f | egrep -v "\.po$")
 
