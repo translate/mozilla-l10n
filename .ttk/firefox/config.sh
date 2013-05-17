dfa1bd91e95d9c45236e62b738c81915c3bf6ee9
Dwayne Bailey <dwayne@translate.org.za>
2013-05-17 16:43:01 +0200

    webapprt is a new product

--- .ttk/firefox/config.sh
+++ .ttk/firefox/config.sh
@@ -10,7 +10,7 @@ precommand=". /var/www/sites/$instance/env/bin/activate;"
 opt_verbose=3
 
 # FIXME we should build this from the get_moz_enUS script
-PRODUCT_DIRS="browser dom netwerk security services/sync toolkit" # Directories in language repositories to clear before running po2moz
+PRODUCT_DIRS="browser dom netwerk security services/sync toolkit webapprt" # Directories in language repositories to clear before running po2moz
 # Directories in language repositories to clear before running po2moz
 RETIRED_PRODUCT_DIRS="other-licenses/branding/firefox extensions/reporter"
 
