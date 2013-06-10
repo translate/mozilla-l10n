4a20c8cacac9e49d8ea4b3dd456a3e34b920f1e6
Dwayne Bailey <dwayne@translate.org.za>
2013-06-10 10:26:59 +0100

    Templates: nope it is still services/sync

--- .ttk/firefox/config.sh
+++ .ttk/firefox/config.sh
@@ -10,9 +10,9 @@ precommand=". /var/www/sites/$instance/env/bin/activate;"
 opt_verbose=3
 
 # FIXME we should build this from the get_moz_enUS script
-PRODUCT_DIRS="browser dom netwerk security sync toolkit webapprt" # Directories in language repositories to clear before running po2moz
+PRODUCT_DIRS="browser dom netwerk security services/sync toolkit webapprt" # Directories in language repositories to clear before running po2moz
 # Directories in language repositories to clear before running po2moz
-RETIRED_PRODUCT_DIRS="other-licenses/branding/firefox extensions/reporter services/sync"
+RETIRED_PRODUCT_DIRS="other-licenses/branding/firefox extensions/reporter"
 
 OTHER_EXCLUDED_DIRS="editor mail thunderbird chat mobile embedding"
 
