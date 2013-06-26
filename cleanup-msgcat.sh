08921bab75baccefa3a953179f4a70204aaa81b6
Dwayne Bailey <dwayne@translate.org.za>
2013-06-26 13:53:23 +0100

    Turned cleanup-msgcat.sh into a ttk-cmd

--- cleanup-msgcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for lang in $*
-do
-	for po in $(find $lang -name "*.po")
-	do
-		msgcat -o $po.2 $po 2> >(egrep -v "internationali[sz]ed messages should not contain the .* escape sequence" >&2) && mv $po.2 $po
-	done
-done
