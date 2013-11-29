2b6da2025da1c2acc67111f2a799ff9b1c3fe9a7
Dwayne Bailey <dwayne@translate.org.za>
2013-11-29 09:27:44 +0000

    Fix target server
    Now that mozilla is on its own server we should point there as pootle.lm.org
    will probably be a different physical server.

--- .ttk/firefox/config.sh
+++ .ttk/firefox/config.sh
@@ -1,7 +1,7 @@
 project=firefox
 instance=mozilla
 user=pootlesync
-server=pootle.locamotion.org
+server=mozilla.locamotion.org
 local_copy=$base_dir/.pootle_tmp
 phaselist=
 manage_command="/var/www/sites/$instance/src/manage.py"
