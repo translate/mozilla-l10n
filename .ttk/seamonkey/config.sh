d7fb2e5e0e72ee4d9272a7c243f698198317d0eb
Mozilla Pootle L10n Robot <tech-team@translate.org.za>
2014-11-19 19:07:00 +0000

    Templates: initialise (seamonkey)

---
+++ .ttk/seamonkey/config.sh
@@ -0,0 +1,22 @@
+project=seamonkey
+instance=mozilla
+user=pootlesync
+server=mozilla.locamotion.org
+local_copy=$base_dir/.pootle_tmp
+phaselist=
+manage_command="/var/www/sites/$instance/src/manage.py"
+manage_py_verbosity=0
+precommand=". /var/www/sites/$instance/env/bin/activate;"
+opt_verbose=0
+
+# FIXME we should build this from the get_moz_enUS script
+PRODUCT_DIRS="suite" # Directories in language repositories to clear before running po2moz
+# Directories in language repositories to clear before running po2moz
+RETIRED_PRODUCT_DIRS=""
+
+OTHER_EXCLUDED_DIRS="browser dom netwerk security services/sync toolkit webapprt mobile embedding calendar chat editor/ui mail other-licenses/branding/thunderbird"
+
+MOZ_PRODUCT=suite
+
+MOZ_REPO=comm-aurora
+L10N_VER=l10n
