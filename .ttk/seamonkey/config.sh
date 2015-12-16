d09c36c37cbdea9a9755e9194af4a578612da5f8
Mozilla Pootle L10n Robot <tech-team@translate.org.za>
2015-12-16 16:55:57 +0000

    Config: add devtools dirs

--- .ttk/seamonkey/config.sh
+++ .ttk/seamonkey/config.sh
@@ -14,7 +14,7 @@ PRODUCT_DIRS="suite" # Directories in language repositories to clear before runn
 # Directories in language repositories to clear before running po2moz
 RETIRED_PRODUCT_DIRS=""
 
-OTHER_EXCLUDED_DIRS="browser dom netwerk security services/sync toolkit webapprt mobile embedding calendar chat editor/ui mail other-licenses/branding/thunderbird"
+OTHER_EXCLUDED_DIRS="browser devtools dom netwerk security services/sync toolkit webapprt mobile embedding calendar chat editor/ui mail other-licenses/branding/thunderbird"
 
 MOZ_PRODUCT=suite
 
