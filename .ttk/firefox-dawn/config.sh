703fb827a002634ba908044554990fd659a7f08b
Mozilla Pootle L10n Robot <tech-team@translate.org.za>
2017-04-10 12:08:07 +0000

    Remove uneeded languages from testing

---
+++ .ttk/firefox-dawn/config.sh
@@ -0,0 +1,25 @@
+project=firefox-dawn
+instance=mozilla
+user=pootlesync
+server=mozilla.locamotion.org
+local_copy=$base_dir/.pootle_tmp
+phaselist=
+manage_command="/var/www/sites/$instance/src/manage.py"
+manage_py_verbosity=0
+precommand=". /var/www/sites/$instance/env/bin/activate;"
+opt_verbose=0
+
+# FIXME we should build this from the get_moz_enUS script
+PRODUCT_DIRS="browser devtools dom netwerk security services/sync toolkit" # Directories in language repositories to clear before running po2moz
+# Directories in language repositories to clear before running po2moz
+RETIRED_PRODUCT_DIRS="other-licenses/branding/firefox extensions/reporter"
+
+OTHER_EXCLUDED_DIRS="editor mail thunderbird chat mobile embedding calendar suite"
+
+MOZ_PRODUCT=browser
+
+MOZ_REPO=mozilla-aurora
+MOZ_DIR="mozilla-aurora"
+L10N_VER=l10n
+
+alt_src="bn_IN es es_MX fr ru"
